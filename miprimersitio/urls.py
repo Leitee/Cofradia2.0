053cef32bf2551c1e590d000b8ad38ece333f1d1
luck2805 <berniluciano@gmail.com>
2015-09-13 11:51:44 -0300

    listar cat - detalle publi - sliders

--- miprimersitio/urls.py
+++ miprimersitio/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     url(r'^$', 'login.views.homepage', name='homepage'),
     url(r'^publi_cat/(?P<publi_id>\d+)/$', 'aplicacion.views.publi_listar', name='publi_listar'),
     url(r'^publi_detalle/(?P<publi_id>\d+)/$', 'aplicacion.views.publi_detalle',name='publi_detalle'),
-    #url(r'^publi/(?P<id>\d+)/$' ), 
+
+
 
 
 ]
